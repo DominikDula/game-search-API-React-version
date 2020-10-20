f6d5252cc10e0318a635e4c68223714db2967bd6
destrojer <dominodula@gmail.com>
2020-10-20 18:06:26 +0200

    added gameinfo component to allgames

--- src/views/AllGames.jsx
+++ src/views/AllGames.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React , {useState ,useEffect} from 'react'
+
+import GameInfo from '../components/game/GameInfo'
+
 
 function AllGames() {
+
+    const [allgames, setallgames] = useState('')
+
+    useEffect(() => {
+        getAllGame()
+    }, [])
+
+
+    async function getAllGame() {
+        try {
+            let response = await fetch(`https://rawg.io/api/games?&page=1`);
+            let data = await response.json()
+            let results = data.results
+
+            setallgames(results)
+            }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
 <div >
     <div className="grid-container">
-        toto je All Games
+        {allgames ? allgames.map(game => (
+            <GameInfo key={game.id} games={game} />
+        )) :null}
+        
     </div>
 
 </div>
     )
 }
+
 
 export default AllGames
